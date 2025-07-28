e1d27954782194feceb69b9acd0290887a287c8b
Abhishek Kumar <ytabhishek2002@gmail.com>
2025-07-28 12:42:09 +0530

    Add files via upload

---
+++ problem4.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class problem4 {
+    public static int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        int result = lengthOfLongestSubstring(s);
+        System.out.println("Length of longest substring without duplicates: " + result);
+    }
+}
